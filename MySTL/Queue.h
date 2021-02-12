268a3c08a97f9cc017bdcdd224304422e8cfaca7
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2021-02-12 20:09:19 +0800

    改进LinkedList性能

---
+++ MySTL/Queue.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "LinkedList.h"
+
+namespace mystl
+{
+	template<typename type>
+	class Queue : LinkedList<type>
+	{
+	public:
+		//将一个元素放入队列末尾
+		void put(type data)
+		{
+		}
+
+		//返回队列第一个元素
+		void peek(int index)
+		{
+		}
+	};
+}
