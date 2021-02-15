a424fdd71fcbfffded37bf92de9abf1acf0e7a7a
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2021-02-15 15:03:15 +0800

    修复Hash算法内存越界导致Hash碰撞的Bug，声明部分需要实现的Map算法

--- MySTL/Map.h
+++ MySTL/Map.h
@@ -4,6 +4,7 @@
 
 namespace mystl
 {
+	//哈希计算
 	template<typename key>
 	class Hash
 	{
@@ -12,11 +13,9 @@ namespace mystl
 		using byte = char;
 
 		//计算哈希值
-		static uint getHashCode(key key)
+		static uint getHashCode(key key, int size)
 		{
 			uint res = 0;
-			//键长度
-			int size = sizeof(key);
 			//对齐后的数组长度
 			int len = size / 4;
 			if (size % 4 != 0)
@@ -25,10 +24,10 @@ namespace mystl
 			}
 
 			byte* str = new byte[len * 4];
-			memset(str, 0, len * 4);
+			//memset(str, 0, len * 4);
 			memcpy(str, &key, size);
-			uint temp;
-			for (int i = len; i > 0; i--)
+			uint temp = 0;
+			for (int i = 0; i < len; i++)
 			{
 				memcpy(&temp, (str + i * 4), 4);
 				res += temp;
@@ -36,6 +35,11 @@ namespace mystl
 
 			delete str;
 			return res;
+		}
+		//计算哈希值
+		static uint getHashCode(key key)
+		{
+			return getHashCode(key, sizeof(key));
 		}
 	};
 
@@ -49,7 +53,86 @@ namespace mystl
 	};
 
 	//哈希表
+	template<typename key, typename value>
 	class Map
 	{
+	protected:
+		using Hash = Hash<key>;
+		using Node = HashNode<key, value>;
+		using List = LinkedList<Node>;
+		using uint = unsigned int;
+
+		//加载因子
+		static const double loadFactor;
+		//容量
+		int size = 11;
+		//当前键值对总数
+		int count = 0;
+		//内部链表指针
+		List* list = nullptr;
+
+		//扩容函数
+		void expand()
+		{
+		}
+	public:
+		//默认构造函数
+		Map()
+		{
+		}
+
+		//指定初始大小的构造函数
+		//大小最小可为11，若不满足该条件则强制执行
+		Map(int size)
+		{
+		}
+
+		//释放资源
+		~Map()
+		{
+		}
+
+		//将一对键值放入哈希表，
+		//若键已存在将会覆盖值，并返回false
+		//否则正常添加返回true
+		bool put(key k, value v)
+		{
+		}
+
+		//根据键获取值
+		//若键存在返回true
+		//否则返回false
+		bool get(key k, value* v)
+		{
+		}
+
+		//是否已存在该键
+		bool containsKey(key key)
+		{
+		}
+
+		//根据键删除键值对
+		//若键存在返回true
+		//否则返回false
+		bool remove(key k)
+		{
+		}
+
+		//清空所有键值对
+		void clear()
+		{
+		}
+
+		//获取当前键值对个数
+		int getCount()
+		{
+		}
+
+		//获取当前容量上限
+		int getSize()
+		{
+		}
 	};
+	template<typename key, typename value>
+	const double Map<key, value>::loadFactor = 0.75f;
 };
--- Test/main.cpp
+++ Test/main.cpp
@@ -5,25 +5,9 @@ using namespace mystl;
 
 int main()
 {
-	auto a = Hash<double>::getHashCode(1);
-	auto b = Hash<double>::getHashCode(2);
-	auto c = Hash<double>::getHashCode(3);
-	auto d = Hash<double>::getHashCode(4);
-	auto e = Hash<double>::getHashCode(5);
-	auto f = Hash<double>::getHashCode(6);
-	auto g = Hash<double>::getHashCode(7);
-	auto h = Hash<double>::getHashCode(8);
-	auto i = Hash<double>::getHashCode(9);
-	auto j = Hash<double>::getHashCode(10);
-	auto k = Hash<double>::getHashCode(11);
-	auto l = Hash<double>::getHashCode(12);
-	auto m = Hash<double>::getHashCode(13);
-	auto n = Hash<double>::getHashCode(14);
-	auto o = Hash<double>::getHashCode(15);
-	auto p = Hash<double>::getHashCode(16);
-	auto q = Hash<double>::getHashCode(17);
-	auto r = Hash<double>::getHashCode(18);
-	auto s = Hash<double>::getHashCode(19);
-	auto t = Hash<double>::getHashCode(20);
+	char hello[] = "lllll";
+	char hello1[] = "lll1l";
+	auto a = Hash<char[]>::getHashCode(hello, sizeof(hello));
+	auto b = Hash<char[]>::getHashCode(hello1, sizeof(hello1));
 	return 0;
 }
