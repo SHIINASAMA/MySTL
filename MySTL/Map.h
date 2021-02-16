b45fba624b1c0fa494570b2852e877003dc83aab
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2021-02-16 15:03:36 +0800

    实现Map基础功能

--- MySTL/Map.h
+++ MySTL/Map.h
@@ -60,6 +60,7 @@ namespace mystl
 		using Hash = Hash<key>;
 		using Node = HashNode<key, value>;
 		using List = LinkedList<Node>;
+		using Iterator = Iterator<Node>;
 		using uint = unsigned int;
 
 		//加载因子
@@ -69,8 +70,9 @@ namespace mystl
 		//当前键值对总数
 		int count = 0;
 		//内部链表指针
-		List* list = nullptr;
+		List** list = nullptr;
 
+		//TODO:待实现
 		//扩容函数
 		void expand()
 		{
@@ -79,7 +81,11 @@ namespace mystl
 		//默认构造函数
 		Map()
 		{
-			this->list = new List[this->size];
+			this->list = new List * [this->size];
+			for (int i = 0; i < this->size; i++)
+			{
+				this->list[i] = new List();
+			}
 		}
 
 		//指定初始大小的构造函数
@@ -95,12 +101,22 @@ namespace mystl
 				this->size = 11;
 			}
 
-			this->list = new List[this->size];
+			this->list = new List * [this->size];
+			for (int i = 0; i < this->size; i++)
+			{
+				this->list[i] = new List();
+			}
 		}
 
 		//释放资源
 		~Map()
 		{
+			this->clear();
+			for (int i = 0; i < this->size; i++)
+			{
+				delete this->list[i];
+			}
+			delete[] this->list;
 		}
 
 		//将一对键值放入哈希表，
@@ -110,13 +126,32 @@ namespace mystl
 		{
 			uint hashCode = Hash::getHashCode(k);
 			int pos = hashCode % this->size;
+			Node tag{ k,v };
 			if (this->list[pos]->getCount() == 0)
 			{
 				//无哈希冲突
+				this->list[pos]->addFirst(tag);
+				this->count++;
+				return true;
 			}
 			else
 			{
 				//存在哈希冲突
+				Iterator* itor = this->list[pos]->getIterator();
+				Node* node;
+				while ((node = itor->next()) != nullptr)
+				{
+					if (node->key == k)
+					{
+						//存在该键，直接覆盖
+						node->value = v;
+						return false;
+					}
+				}
+				//不存在改键，新增
+				this->list[pos]->addFirst(tag);
+				this->count++;
+				return true;
 			}
 		}
 
@@ -125,11 +160,52 @@ namespace mystl
 		//否则返回false
 		bool get(key k, value* v)
 		{
+			uint hashCode = Hash::getHashCode(k);
+			int pos = hashCode % this->size;
+			if (this->list[pos]->getCount() == 0)
+			{
+				v = nullptr;
+				return false;
+			}
+			else
+			{
+				Iterator* itor = this->list[pos]->getIterator();
+				Node* node;
+				while ((node = itor->next()) != nullptr)
+				{
+					if (node->key == k)
+					{
+						*v = node->value;
+						return true;
+					}
+				}
+				v = nullptr;
+				return false;
+			}
 		}
 
 		//是否已存在该键
 		bool containsKey(key key)
 		{
+			uint hashCode = Hash::getHashCode(key);
+			int pos = hashCode % this->size;
+			if (this->list[pos]->getCount() == 0)
+			{
+				return false;
+			}
+			else
+			{
+				Iterator* itor = this->list[pos]->getIterator();
+				Node* node;
+				while ((node = itor->next()) != nullptr)
+				{
+					if (node->key == key)
+					{
+						return true;
+					}
+				}
+				return false;
+			}
 		}
 
 		//根据键删除键值对
@@ -137,23 +213,54 @@ namespace mystl
 		//否则返回false
 		bool remove(key k)
 		{
+			uint hashCode = Hash::getHashCode(k);
+			int pos = hashCode % this->size;
+			if (this->list[pos]->getCount() == 0)
+			{
+				return false;
+			}
+			else
+			{
+				Iterator* itor = this->list[pos]->getIterator();
+				Node* node;
+				int i = 0;
+				while ((node = itor->next()) != nullptr)
+				{
+					if (node->key == k)
+					{
+						this->list[pos]->remove(i);
+						this->count--;
+						return true;
+					}
+					i++;
+				}
+				return false;
+			}
 		}
 
 		//清空所有键值对
 		void clear()
 		{
+			for (int i = 0; i < this->size; i++)
+			{
+				this->list[i]->clear();
+			}
+			this->count = 0;
 		}
 
 		//获取当前键值对个数
 		int getCount()
 		{
+			return this->count;
 		}
 
 		//获取当前容量上限
 		int getSize()
 		{
+			return this->size;
 		}
 	};
+
 	template<typename key, typename value>
 	const double Map<key, value>::loadFactor = 0.75f;
 };
--- Test/main.cpp
+++ Test/main.cpp
@@ -5,15 +5,6 @@ using namespace mystl;
 
 int main()
 {
-	LinkedList<int>* list = new LinkedList<int>;
-	list->addFirst(100);
-	list->addFirst(101);
-	list->addFirst(102);
-	auto itor = list->getIterator();
-	int* a;
-	while ((a = itor->next()) != nullptr)
-	{
-		printf("a = %d\n", *a);
-	}
+	Map<int, int>* map = new Map<int, int>;
 	return 0;
 }
